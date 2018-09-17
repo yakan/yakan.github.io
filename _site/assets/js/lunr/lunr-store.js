ffd1278a85f511fc5876813a917cf24720e3096f
yakan <fath.yakan@gmail.com>
2018-09-17 00:02:36 -0700

    homeless tracker

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Interactive Homeless Tracker Dashboard for Los Angeles City and County",
         "excerpt":"Project Goal The goal of this project is to provide insightful geographic, demographic, and time indicators of homelessness in The City of LA. All the reports in our dashboard are integrated into an online mapping application that will enable Mayor’s office staff to evaluate and track the effects of existing...","categories": [],
         "tags": ["data science","exploratory analysis","RStudio","R","tidyverse","shiny","leaflet","City of LA","social science"],
-        "url": "http://localhost:4000/interactive_homeless_tracker/",
+        "url": "/interactive_homeless_tracker/",
         "teaser":null}]
